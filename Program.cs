1caa30cfd17e6c01b87a29387b059e9fa154f65d
alexb <alexb@ALFA>
2025-02-04 11:51:02 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introdusca un numero para repetir la frase: Hello world");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i + ".-" + " " + "hello wordl");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
